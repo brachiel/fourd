357fc9b959f2bdded98ef45dc2cbd1acf186e3b5
brachiel <brachiel@github.invalid>
2017-10-02 23:10:06 +0200

    Working rotating 3d cube

--- src/Graph.hs
+++ src/Graph.hs
@@ -5,11 +5,13 @@ module Graph
 import Graphics.Gloss ( Display(..), Picture(..)
                       , animate, white
                       )
-import Data.Vect (mkVec2, Vec2 (..))
+import Data.Vect ( mkVec2, Vec2 (..)
+                 , mkVec3, Vec3 (..)
+                 )
 import qualified Tesseract as T
 
 animationSpeed :: Float
-animationSpeed = 0.1
+animationSpeed = 0.3
 
 display :: Display
 display = InWindow "FourD" (400, 400) (100, 100)
@@ -18,11 +20,21 @@ runGraph :: IO ()
 runGraph = animate display white animation
 
 animation :: Float -> Picture
-animation a = picturePolygon2 $ T.square 80 (mkVec2 (0,0)) rot
-              where rot = (animationSpeed*a)
-
-picturePolygon2 :: T.Polygon Vec2 -> Picture
-picturePolygon2 (T.Polygon vs) = Line $ map unbox (vs ++ [head vs])
-                                where unbox (Vec2 x y) = (x,y)
+animation s = polygon3ToPicture $ animationCube (animationSpeed*s)
+--animation s = polygon2ToPicture $ animationSquare (animationSpeed*s)
 
 
+animationSquare :: Float -> T.Polygon Vec2
+animationSquare = T.square 80 (mkVec2 (0,0))
+
+
+animationCube :: Float -> [T.Polygon Vec3]
+animationCube r = T.cube 80 (mkVec3 (0,0,0)) (T.Rot3 (mkVec3 (0.2,0.2,0.2)) r)
+
+
+polygon2ToPicture :: T.Polygon Vec2 -> Picture
+polygon2ToPicture (T.Polygon vs) = Line $ map unbox (vs ++ [head vs])
+                                   where unbox (Vec2 x y) = (x,y)
+
+polygon3ToPicture :: [T.Polygon Vec3] -> Picture
+polygon3ToPicture vs = Pictures $ map (polygon2ToPicture . T.proj) vs
--- src/Tesseract.hs
+++ src/Tesseract.hs
@@ -16,15 +16,31 @@ instance Embeddable3 (Polygon Vec2) where
     embed z = fmap embedV
             where embedV (Vec2 x y) = Vec3 x y z
 
+class Projectable2 a where
+    proj :: a -> Polygon Vec2
+
+instance Projectable2 (Polygon Vec3) where
+    proj = fmap $ dropZ . flip project (Vec3 0 0 1)
+           where dropZ (Vec3 x y _) = Vec2 x y
+
 -- https://en.wikipedia.org/wiki/Rotations_in_4-dimensional_Euclidean_space
 
 square :: Float -> Vec2 -> Float -> Polygon Vec2
-square length center rotation = Polygon $ map ((center &+) . (length/2 *&) . rotate2 rotation) $ structVec2 [-1,-1, -1,1, 1,1, 1,-1]
+square length center rotation = Polygon $ map ((center &+) . (length/2 *&) . rotate2 rotation) unitSquare
+                                where unitSquare = structVec2 [-1,-1, -1,1, 1,1, 1,-1]
 
+-- |Rotate a 3d polygon by the angles a and b around the x and the y axis.
+-- where for the angles a and b holds: 1 = 90°, 2 = 180°
+rotate3d :: (Float, Float) -> Polygon Vec3 -> Polygon Vec3
+rotate3d (a,b) = fmap $ rotate3 (pi/2*b) (Vec3 0 1 0) . rotate3 (pi/2*a) (Vec3 1 0 0)
+
+-- |A cube of length, position and rotation represented by a list of Vec3 polygons
 cube :: Float -> Vec3 -> Rot3 -> [Polygon Vec3]
-cube length center (Rot3 axis rotation) = sides
-                                          where sides = rotators <*> [side]
-                                                rotators = ([rotate] <*> rotations) :: [Polygon Vec3 -> Polygon Vec3]
-                                                rotate (a,b) = fmap $ rotate3 b (Vec3 0 1 0) . rotate3 a (Vec3 1 0 0) :: (Float, Float) -> Polygon Vec3 -> Polygon Vec3
+cube length center (Rot3 axis rotation) = fmap (fmap $ rotate3 rotation axis) sides
+                                          where sides = rotators <*> [side] :: [Polygon Vec3]
+                                                rotators = ([rotate3d] <*> rotations) :: [Polygon Vec3 -> Polygon Vec3]
                                                 rotations = [(0,0),(-1,0),(1,0),(2,0),(0,-1),(0,1)] :: [(Float,Float)]
-                                                side = embed 1 $ square length (Vec2 0 0) 0 :: Polygon Vec3
+                                                -- Creates a side which is then replicated and rotated around
+                                                side = embed (length/2) $ square length (Vec2 0 0) 0 :: Polygon Vec3
+
+
