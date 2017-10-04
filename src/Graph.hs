15caf694e8b0fa6d66258dc6d8433f7cc1a0c043
brachiel <brachiel@github.invalid>
2017-10-04 23:15:34 +0200

    Added tesseracts

--- src/Graph.hs
+++ src/Graph.hs
@@ -7,6 +7,8 @@ import Graphics.Gloss ( Display(..), Picture(..)
                       )
 import Data.Vect ( mkVec2, Vec2 (..)
                  , mkVec3, Vec3 (..)
+                 , mkVec4, Vec4 (..)
+                 , Extend
                  )
 import qualified Tesseract as T
 
@@ -20,17 +22,16 @@ runGraph :: IO ()
 runGraph = animate display white animation
 
 animation :: Float -> Picture
-animation s = body3ToPicture $ animationCube (animationSpeed*s)
+animation s = body4ToPicture $ animationTesseract (animationSpeed*s)
+--animation s = body3ToPicture $ animationCube (animationSpeed*s)
 --animation s = polygon2ToPicture $ animationSquare (animationSpeed*s)
-
-
-animationSquare :: Float -> T.Polygon Vec2
-animationSquare = T.square 80 (mkVec2 (0,0))
 
 
 animationCube :: Float -> T.Body Vec3
 animationCube r = T.transformedCube 80 (mkVec3 (0,0,0)) (T.Rot3 (mkVec3 (0.2,0.2,0.2)) r)
 
+animationTesseract :: Float -> T.Body Vec4
+animationTesseract r = T.transformedTesseract 80 (mkVec4 (0,0,0,0)) (T.Rot4 (0.2,0.2*r,r))
 
 polygon2ToPicture :: T.Polygon Vec2 -> Picture
 polygon2ToPicture (T.Polygon vs) = Line $ map unbox (vs ++ [head vs])
@@ -38,4 +39,7 @@ polygon2ToPicture (T.Polygon vs) = Line $ map unbox (vs ++ [head vs])
 
 body3ToPicture :: T.Body Vec3 -> Picture
 body3ToPicture (T.Body vs) = Pictures $ map (polygon2ToPicture . T.flatten) vs
+
+body4ToPicture :: T.Body Vec4 -> Picture
+body4ToPicture (T.Body vs) = Pictures $ map (polygon2ToPicture . T.flatten) vs
 
--- src/Tesseract.hs
+++ src/Tesseract.hs
@@ -12,6 +12,8 @@ data Body a = Body [Polygon a] deriving (Show, Functor)
 data Rot3 = Rot3 Vec3 Float
 noRot3 = Rot3 (Vec3 0 0 0) 0
 
+data Rot4 = Rot4 (Float, Float, Float)
+
 instance (Extend a b, Functor f) => Extend (f a) (f b) where
     extendZero = extendWith 0
     extendWith z = fmap (extendWith z)
@@ -24,37 +26,60 @@ class Flattable f a where
 instance Functor f => Flattable f Vec3 where
     flatten = trim
 
+instance Flattable Polygon Vec4 where
+    flatten = trim . (trim :: Polygon Vec4 -> Polygon Vec3)
+
 instance Flattable Body Vec4 where
     flatten = trim . (trim :: Body Vec4 -> Body Vec3)
 
 -- https://en.wikipedia.org/wiki/Rotations_in_4-dimensional_Euclidean_space
 
-square :: Float -> Vec2 -> Float -> Polygon Vec2
-square length center rotation 
-    = Polygon $ map ((center &+) . (length/2 *&) . rotate2 rotation) unitSquare
-      where unitSquare = structVec2 [-1,-1, -1,1, 1,1, 1,-1]
+-- |A unit square centered at (0 0)
+square :: Polygon Vec2
+square = Polygon $ structVec2 $ map (/2) [-1,-1, -1,1, 1,1, 1,-1]
 
 -- |Rotate a 3d polygon by the angles a and b around the x and the y axis.
 -- where for the angles a and b holds: 1 = 90°, 2 = 180°
 rotate3xy :: (Float, Float) -> Vec3 -> Vec3
-rotate3xy (a,b) = rotate3 (pi/2*b) (Vec3 0 1 0) 
-                . rotate3 (pi/2*a) (Vec3 1 0 0)
+rotate3xy (a,b) = rotate3 (pi/2*b) vec3Y
+                . rotate3 (pi/2*a) vec3X
 
-translate3 :: Vec3 -> Vec3 -> Vec3
-translate3 = (&+)
 
-stretch3 :: Float -> Vec3 -> Vec3
-stretch3 = (*&)
+translate :: Vector v => v -> v -> v
+translate = (&+)
 
--- |A cube of length, position and rotation represented by a list of Vec3 
+stretch :: Vector v => Float -> v -> v
+stretch = (*&)
+
+rotate4xyz :: (Float, Float, Float) -> Vec4 -> Vec4
+rotate4xyz (a,b,c) = rotate4 (pi/2*c) (vec4Z, vec4W)
+                   . rotate4 (pi/2*b) (vec4Y, vec4W)
+                   . rotate4 (pi/2*a) (vec4X, vec4W)
+
+-- |A unit cube, centered at (0 0 0)
 -- polygons
 cube :: Body Vec3
 cube = Body $ rotators <*> [side]
        where rotators = map fmap $ [rotate3xy] <*> rotations
              rotations = [(0,0),(-1,0),(1,0),(2,0),(0,-1),(0,1)]
              -- Creates a side which is then replicated and rotated around
-             side = extendWith (1/2) $ square 1 (Vec2 0 0) 0 
+             side = extendWith (1/2) square
 
 transformedCube :: Float -> Vec3 -> Rot3 -> Body Vec3
-transformedCube x v (Rot3 a r) = fmap (rotate3 r a . translate3 v . stretch3 x) $ cube
+transformedCube x v (Rot3 a r) = fmap (rotate3 r a . translate v . stretch x) cube
+
+-- |A unit tesseract, centered at (0 0 0 0)
+tesseract :: Body Vec4
+tesseract = Body $ rotators <*> sidePolys
+            where rotators = (fmap fmap ([rotate4xyz] <*> rotations)) :: [Polygon Vec4 -> Polygon Vec4]
+                  rotations = [(0,0,0)
+                              ,(0,0,1) ,(0,1,0) ,(1,0,0)
+                              ,(0,0,-1),(0,-1,0),(-1,0,0)
+                              ,(0,0,2)]
+                  -- Creates a side which is then replicated and rotated around
+                  Body sidePolys = extendWith (1/2) $ cube
+
+
+transformedTesseract :: Float -> Vec4 -> Rot4 -> Body Vec4
+transformedTesseract x v (Rot4 r) = fmap (rotate4xyz r . translate v . stretch x) tesseract
 
